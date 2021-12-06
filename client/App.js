07be6adc4fe0d9558ed5eb00f95427470db849eb
ElManchacho <pld.pro@hotmail.com>
2021-12-06 18:43:00 +0100

    envVariables

--- client/App.js
+++ client/App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
-  const fetchUser = async (username) => {
-    const response = await fetch(`http://87cd-86-218-25-162.ngrok.io/api/users/${username}`);
-    const data = await response.json();
 
-    console.log(data);
+  const [text, onChangeText] = React.useState("Useless Text");
+
+  const [myText, setMyText] = useState("My Original Text");
+
+  let userData = ""
+
+  const fetchUser = async (username) => {
+    //const response = await fetch(process.env.NGROK_URL + `/api/users/${username}`);
+    console.log("url :")
+    console.log(process.env.GITHUB_URL)
+    const response = await fetch(`${process.env.GITHUB_URL}+${username}`,{
+      headers:{
+        Authorization : "token " + process.env.API_TOKEN
+      }
+    });
+    const data = await response.json();
+    if (data.login)
+    {
+      userData = "login : "+data.login+", id : "+data.id 
+    }
+    else
+    {
+      userData = data.message
+    }
   }
 
-  fetchUser("Test");
+  fetchUser(text)
 
   return (
     <View style={styles.container}>
-      <Text>Hello !</Text>
+      <Text>Hello ! You can search a user's github data here : </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text onPress = {() => setMyText(userData)}>
+                    {myText}
+            </Text>
       <StatusBar style="auto" />
     </View>
   );
